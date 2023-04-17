cf99e34471527276020fd9d000c725706c651943
krishkcompanysec <65386127+krishkcompanysec@users.noreply.github.com>
2023-04-17 23:00:41 +0530

    Add files via upload

---
+++ index.js.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 3030;
+
+// your code
+
+app.listen(PORT, () => {
+  console.log('server started on port ${PORT}');
+console.log('navigator.userAgent')
+console.log(navigator.userAgent)
+});
