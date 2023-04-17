981cd08af5e15963964a91fb7d47013e0b8947d1
krishkcompanysec <65386127+krishkcompanysec@users.noreply.github.com>
2023-04-17 23:37:36 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,8 +4,13 @@ const PORT = process.env.PORT || 3030;
 
 // your code
 
+const UserAgent = require('user-agents'); 
+   
+const userAgent = new UserAgent();
+
+
+
 app.listen(PORT, () => {
   console.log('server started on port ${PORT}');
-console.log('navigator.userAgent')
-console.log(navigator.userAgent)
+console.log(userAgent.toString());
 });
